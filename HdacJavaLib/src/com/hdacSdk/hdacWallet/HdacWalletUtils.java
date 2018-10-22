8516a5a8f220377d51b33615f42f56de67848cdc
MrDisarm0 <33016274+MrDisarm0@users.noreply.github.com>
2018-10-22 12:45:18 +0900

    add wallet utils
    add hex string <-> Bytes utils

--- HdacJavaLib/src/com/hdacSdk/hdacWallet/HdacWalletUtils.java
+++ HdacJavaLib/src/com/hdacSdk/hdacWallet/HdacWalletUtils.java
@@ -96,5 +96,29 @@ public class HdacWalletUtils {
         else return UNKNOWN_OS;
         
 	}
+	
+	public static byte[] hexToBytes(String hex) { 
+		if (hex == null || hex.length() == 0) {
+			return null; 
+		} 
+		byte[] ba = new byte[hex.length() / 2]; 
+		for (int i = 0; i < ba.length; i++) {
+			ba[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16); 
+		} 
+		return ba; 
+	} 
+	
+	public static String bytesToHex(byte[] ba) {
+		if (ba == null || ba.length == 0) {
+			return null; 
+		} 
+		StringBuffer sb = new StringBuffer(ba.length * 2); 
+		String hexNumber; 
+		for (int x = 0; x < ba.length; x++) {
+			hexNumber = "0" + Integer.toHexString(0xff & ba[x]);
+			sb.append(hexNumber.substring(hexNumber.length() - 2));
+		}
+		return sb.toString(); 
+	} 
 
 }
