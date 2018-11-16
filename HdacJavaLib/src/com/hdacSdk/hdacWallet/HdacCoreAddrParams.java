c2a993136f0c650c8620fbe127a528e9bf9ff518
MrDisarm0 <33016274+MrDisarm0@users.noreply.github.com>
2018-11-16 13:43:11 +0900

    update for private core parameters
    Data type of addressHeader, p2shHeader is change to string

--- HdacJavaLib/src/com/hdacSdk/hdacWallet/HdacCoreAddrParams.java
+++ HdacJavaLib/src/com/hdacSdk/hdacWallet/HdacCoreAddrParams.java
@@ -1,5 +1,7 @@
 package com.hdacSdk.hdacWallet;
 
+import java.math.BigInteger;
+
 /*
  * Referred values of params.dat in a core 
  * address-pubkeyhash-version         # Version bytes used for pay-to-pubkeyhash addresses.
@@ -16,15 +18,20 @@ package com.hdacSdk.hdacWallet;
  */
 public class HdacCoreAddrParams {	
 	
-	public int addressHeader; // pubkeyhash
-	public int p2shHeader;    // scripthash        
+	public String addressHeader; // pubkeyhash
+	public String p2shHeader;    // scripthash  
 	public String addressChecksumValue;
-    
-	public HdacCoreAddrParams(int addressHeader, int p2shHeader, String addressChecksumValue) {
+	
+	/**
+	 * @code Custom Net : Be able to use specific data by user 
+	 * Input parameters( addressHeader, p2shHeader, addressChecksum) of custom net 
+	 */
+	
+	public HdacCoreAddrParams(String addressHeader, String p2shHeader, String addressChecksumValue) {
 		this.addressHeader = addressHeader;
 		this.p2shHeader = p2shHeader;
 		this.addressChecksumValue = addressChecksumValue;
-	}	
+	}
     
 	public HdacCoreAddrParams(boolean publicNet){
 		/**
@@ -33,12 +40,12 @@ public class HdacCoreAddrParams {
 		 * If public, the first character of the generated address is "H"
 		 */
 		if(!publicNet) {
-    		this.addressHeader = 0x64;    	
-    		this.p2shHeader = 0x08;        
+    		this.addressHeader = "64";
+    		this.p2shHeader = "08";
     		this.addressChecksumValue = "48545354";
     	}else {
-    		this.addressHeader = 0x28;    	
-    		this.p2shHeader = 0x08;        
+    		this.addressHeader = "28";
+    		this.p2shHeader = "08";
     		this.addressChecksumValue = "48444143";
     	}
 		/**
@@ -46,19 +53,19 @@ public class HdacCoreAddrParams {
 		 */
     }
     
-    public int getAddressHeader() {
+    public String getAddressHeader() {
 		return addressHeader;
 	}
 
-	public void setAddressHeader(int verPubkeyhash) {
+	public void setAddressHeader(String verPubkeyhash) {
 		this.addressHeader = verPubkeyhash;
-	}
+	}	
 
-	public int getP2shHeader() {
+	public String getP2shHeader() {
 		return p2shHeader;
 	}
 
-	public void setP2shHeader(int verScripthash) {
+	public void setP2shHeader(String verScripthash) {
 		this.p2shHeader = verScripthash;
 	}
 
